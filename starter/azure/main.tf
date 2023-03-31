7adc6d13205f0573a43c3b8eabda80d0f8f5a49f
Nikos Kordis <nikoskordis@gmail.com>
2023-03-31 16:10:51 +0200

    Edit the environment variables

--- starter/azure/main.tf
+++ starter/azure/main.tf
@@ -1,5 +1,6 @@
 data "azurerm_resource_group" "udacity" {
-  name     = "Regroup_4gKqrgD_cn"
+  //name     = "Regroup_4gKqrgD_cn"
+  name = "Regroup_9peEuUpMCkYEv31"
 }
 
 resource "azurerm_container_group" "udacity" {
@@ -7,7 +8,7 @@ resource "azurerm_container_group" "udacity" {
   location            = data.azurerm_resource_group.udacity.location
   resource_group_name = data.azurerm_resource_group.udacity.name
   ip_address_type     = "Public"
-  dns_name_label      = "udacity-tscotto-azure"
+  dns_name_label      = "udacity-nikosko-azure"
   os_type             = "Linux"
 
   container {
@@ -16,8 +17,8 @@ resource "azurerm_container_group" "udacity" {
     cpu    = "0.5"
     memory = "1.5"
     environment_variables = {
-      "AWS_S3_BUCKET"       = "udacity-tscotto-aws-s3-bucket",
-      "AWS_DYNAMO_INSTANCE" = "udacity-tscotto-aws-dynamodb"
+      "AWS_S3_BUCKET"       = "udacity-nikosko-aws-s3-bucket",
+      "AWS_DYNAMO_INSTANCE" = "udacity-nikosko-aws-dynamodb"
     }
     ports {
       port     = 3000
@@ -30,3 +31,37 @@ resource "azurerm_container_group" "udacity" {
 }
 
 ####### Your Additions Will Start Here ######
+
+resource "azurerm_storage_account" "example" {
+  name                     = "examplesa"
+  resource_group_name      = data.azurerm_resource_group.udacity.name
+  location                 = data.azurerm_resource_group.udacity.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+resource "azurerm_mssql_server" "example" {
+  name                         = "example-sqlserver"
+  resource_group_name          = data.azurerm_resource_group.udacity.name
+  location                     = data.azurerm_resource_group.udacity.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_service_plan" "example" {
+  name                = "example"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
+  os_type             = "Linux"
+  sku_name            = "P1v2"
+}
+
+resource "azurerm_linux_web_app" "example" {
+  name                = "example"
+  resource_group_name = data.azurerm_resource_group.udacity.name
+  location            = data.azurerm_resource_group.udacity.location
+  service_plan_id     = azurerm_service_plan.example.id
+
+  site_config {}
+}
