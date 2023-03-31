5ce4d646b759699b1d75ab18f92d5414cd2c42c5
Nikos Kordis <nikoskordis@gmail.com>
2023-03-31 16:26:19 +0200

    Update DynamoDB table

--- starter/aws/main.tf
+++ starter/aws/main.tf
@@ -208,18 +208,46 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "udacity-nikosko-aws-s3-bucket"
 }
 
-resource "aws_dynamodb_table" "my_table" {
-  name           = "udacity-nikosko-aws-dynamodb"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "id"
+resource "aws_dynamodb_table" "basic-dynamodb-table" {
+  name           = "GameScores"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
 
   attribute {
-    name = "id"
+    name = "UserId"
     type = "S"
   }
 
   attribute {
-    name = "name"
+    name = "GameTitle"
     type = "S"
+  }
+
+  attribute {
+    name = "TopScore"
+    type = "N"
+  }
+
+  ttl {
+    attribute_name = "TimeToExist"
+    enabled        = false
+  }
+
+  global_secondary_index {
+    name               = "GameTitleIndex"
+    hash_key           = "GameTitle"
+    range_key          = "TopScore"
+    write_capacity     = 10
+    read_capacity      = 10
+    projection_type    = "INCLUDE"
+    non_key_attributes = ["UserId"]
+  }
+
+  tags = {
+    Name        = "dynamodb-table-1"
+    Environment = "production"
   }
 }
